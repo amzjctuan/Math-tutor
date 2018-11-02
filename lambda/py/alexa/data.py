e214e2fed25ef453f08de162bf274e5cd8898d7d
amzjctuan <43017909+amzjctuan@users.noreply.github.com>
2018-11-02 16:35:00 -0400

    Update data.py
    modify to math questions

--- lambda/py/alexa/data.py
+++ lambda/py/alexa/data.py
@@ -2,274 +2,104 @@
 
 STATES_LIST = [
     {
-        'abbreviation': 'MS', 'capital': 'Jackson',
-        'state': 'Mississippi', 'statehood_order': '20',
-        'statehood_year': '1817'
+        'abbreviation': '2', 'answer': '2',
+        'state': 'one_plus_one', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '4',
+        'state': 'two_plus_two', 'capital': '4',
+        'statehood_year': '4'
     },
     {
-        'abbreviation': 'OK', 'capital': 'Oklahoma City',
-        'state': 'Oklahoma', 'statehood_order': '46',
-        'statehood_year': '1907'
+        'abbreviation': '2', 'answer': '6',
+        'state': 'three_plus_three', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '8',
+        'state': 'four_plus_four', 'capital': '4',
+        'statehood_year': '4'
     },
     {
-        'abbreviation': 'DE', 'capital': 'Dover',
-        'state': 'Delaware', 'statehood_order': '1',
-        'statehood_year': '1787'
+        'abbreviation': '2', 'answer': '10',
+        'state': 'five_plus_five', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '12',
+        'state': 'six_plus_six', 'capital': '4',
+        'statehood_year': '4'
     },
     {
-        'abbreviation': 'MN', 'capital': 'St. Paul',
-        'state': 'Minnesota', 'statehood_order': '32',
-        'statehood_year': '1858'
+        'abbreviation': '2', 'answer': '29',
+        'state': 'nineteen_plus_ten', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '40',
+        'state': 'twenty_plus_twenty', 'capital': '4',
+        'statehood_year': '4'
     },
     {
-        'abbreviation': 'NH', 'capital': 'Concord',
-        'state': 'Hampshire', 'statehood_order': '9',
-        'statehood_year': '1788'
+        'abbreviation': '2', 'answer': '132',
+        'state': 'one_hundred_plus_thirty_two', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '4',
+        'state': 'two_times_two', 'capital': '4',
+        'statehood_year': '4'
     },
     {
-        'abbreviation': 'IL', 'capital': 'Springfield',
-        'state': 'Illinois', 'statehood_order': '21',
-        'statehood_year': '1818'
+        'abbreviation': '2', 'answer': '99',
+        'state': 'nine_times_eleven', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '28',
+        'state': 'fourty_minus_twelve', 'capital': '4',
+        'statehood_year': '4'
     },
     {
-        'abbreviation': 'AR', 'capital': 'Little Rock',
-        'state': 'Arkansas', 'statehood_order': '25',
-        'statehood_year': '1836'
+        'abbreviation': '2', 'answer': '121',
+        'state': 'eleven_times_eleven', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '91',
+        'state': 'seven_times_thirteen', 'capital': '4',
+        'statehood_year': '4'
     },
     {
-        'abbreviation': 'IN', 'capital': 'Indianapolis',
-        'state': 'Indiana', 'statehood_order': '19',
-        'statehood_year': '1816'
-    },
+        'abbreviation': '2', 'answer': '66',
+        'state': 'eighty_minus_fourtten', 'capital': '2',
+        'statehood_year': '2'
+    },   
     {
-        'abbreviation': 'MD', 'capital': 'Annapolis',
-        'state': 'Maryland', 'statehood_order': '7',
-        'statehood_year': '1788'
+        'abbreviation': '4', 'answer': '11',
+        'state': 'fifteen_minus_four', 'capital': '4',
+        'statehood_year': '4'
     },
-    {
-        'abbreviation': 'LA', 'capital': 'Baton Rouge',
-        'state': 'Louisiana', 'statehood_order': '18',
-        'statehood_year': '1812'
-    },
-    {
-        'abbreviation': 'ID', 'capital': 'Boise',
-        'state': 'Idaho', 'statehood_order': '43',
-        'statehood_year': '1890'
-    },
-    {
-        'abbreviation': 'WY', 'capital': 'Cheyenne',
-        'state': 'Wyoming', 'statehood_order': '44',
-        'statehood_year': '1890'
-    },
-    {
-        'abbreviation': 'TN', 'capital': 'Nashville',
-        'state': 'Tennessee', 'statehood_order': '16',
-        'statehood_year': '1796'
-    },
-    {
-        'abbreviation': 'AZ', 'capital': 'Phoenix',
-        'state': 'Arizona', 'statehood_order': '48',
-        'statehood_year': '1912'
-    },
-    {
-        'abbreviation': 'IA', 'capital': 'Des Moines',
-        'state': 'Iowa', 'statehood_order': '29',
-        'statehood_year': '1846'
-    },
-    {
-        'abbreviation': 'MI', 'capital': 'Lansing',
-        'state': 'Michigan', 'statehood_order': '26',
-        'statehood_year': '1837'
-    },
-    {
-        'abbreviation': 'KS', 'capital': 'Topeka',
-        'state': 'Kansas', 'statehood_order': '34',
-        'statehood_year': '1861'
-    },
-    {
-        'abbreviation': 'UT', 'capital': 'Salt Lake City',
-        'state': 'Utah', 'statehood_order': '45',
-        'statehood_year': '1896'
-    },
-    {
-        'abbreviation': 'VA', 'capital': 'Richmond',
-        'state': 'Virginia', 'statehood_order': '10',
-        'statehood_year': '1788'
-    },
-    {
-        'abbreviation': 'OR', 'capital': 'Salem',
-        'state': 'Oregon', 'statehood_order': '33',
-        'statehood_year': '1859'
-    },
-    {
-        'abbreviation': 'CT', 'capital': 'Hartford',
-        'state': 'Connecticut', 'statehood_order': '5',
-        'statehood_year': '1788'
-    },
-    {
-        'abbreviation': 'MT', 'capital': 'Helena',
-        'state': 'Montana', 'statehood_order': '41',
-        'statehood_year': '1889'
-    },
-    {
-        'abbreviation': 'CA', 'capital': 'Sacramento',
-        'state': 'California', 'statehood_order': '31',
-        'statehood_year': '1850'
-    },
-    {
-        'abbreviation': 'MA', 'capital': 'Boston',
-        'state': 'Massachusetts', 'statehood_order': '6',
-        'statehood_year': '1788'
-    },
-    {
-        'abbreviation': 'WV', 'capital': 'Charleston',
-        'state': 'West Virginia', 'statehood_order': '35',
-        'statehood_year': '1863'
-    },
-    {
-        'abbreviation': 'SC', 'capital': 'Columbia',
-        'state': 'South Carolina', 'statehood_order': '8',
-        'statehood_year': '1788'
-    },
-    {
-        'abbreviation': 'WI', 'capital': 'Madison',
-        'state': 'Wisconsin', 'statehood_order': '30',
-        'statehood_year': '1848'
-    },
-    {
-        'abbreviation': 'VT', 'capital': 'Montpelier',
-        'state': 'Vermont', 'statehood_order': '14',
-        'statehood_year': '1791'
-    },
-    {
-        'abbreviation': 'GA', 'capital': 'Atlanta',
-        'state': 'Georgia', 'statehood_order': '4',
-        'statehood_year': '1788'
-    },
-    {
-        'abbreviation': 'ND', 'capital': 'Bismarck',
-        'state': 'North Dakota', 'statehood_order': '39',
-        'statehood_year': '1889'
-    },
-    {
-        'abbreviation': 'PA', 'capital': 'Harrisburg',
-        'state': 'Pennsylvania', 'statehood_order': '2',
-        'statehood_year': '1787'
-    },
-    {
-        'abbreviation': 'FL', 'capital': 'Tallahassee',
-        'state': 'Florida', 'statehood_order': '27',
-        'statehood_year': '1845'
-    },
-    {
-        'abbreviation': 'AK', 'capital': 'Juneau',
-        'state': 'Alaska', 'statehood_order': '49',
-        'statehood_year': '1959'
-    },
-    {
-        'abbreviation': 'KY', 'capital': 'Frankfort',
-        'state': 'Kentucky', 'statehood_order': '15',
-        'statehood_year': '1792'
-    },
-    {
-        'abbreviation': 'HI', 'capital': 'Honolulu',
-        'state': 'Hawaii', 'statehood_order': '50',
-        'statehood_year': '1959'
-    },
-    {
-        'abbreviation': 'NE', 'capital': 'Lincoln',
-        'state': 'Nebraska', 'statehood_order': '37',
-        'statehood_year': '1867'
-    },
-    {
-        'abbreviation': 'MO', 'capital': 'Jefferson City',
-        'state': 'Missouri', 'statehood_order': '24',
-        'statehood_year': '1821'
-    },
-    {
-        'abbreviation': 'OH', 'capital': 'Columbus',
-        'state': 'Ohio', 'statehood_order': '17',
-        'statehood_year': '1803'
-    },
-    {
-        'abbreviation': 'AL', 'capital': 'Montgomery',
-        'state': 'Alabama', 'statehood_order': '22',
-        'statehood_year': '1819'
-    },
-    {
-        'abbreviation': 'RI', 'capital': 'Providence',
-        'state': 'Rhode Island', 'statehood_order': '13',
-        'statehood_year': '1790'
-    },
-    {
-        'abbreviation': 'NJ', 'capital': 'Trenton',
-        'state': 'Jersey', 'statehood_order': '3',
-        'statehood_year': '1787'
-    },
-    {
-        'abbreviation': 'SD', 'capital': 'Pierre',
-        'state': 'South Dakota', 'statehood_order': '40',
-        'statehood_year': '1889'
-    },
-    {
-        'abbreviation': 'CO', 'capital': 'Denver',
-        'state': 'Colorado', 'statehood_order': '38',
-        'statehood_year': '1876'
-    },
-    {
-        'abbreviation': 'NM', 'capital': 'Santa Fe',
-        'state': 'Mexico', 'statehood_order': '47',
-        'statehood_year': '1912'
-    },
-    {
-        'abbreviation': 'WA', 'capital': 'Olympia',
-        'state': 'Washington', 'statehood_order': '42',
-        'statehood_year': '1889'
-    },
-    {
-        'abbreviation': 'NC', 'capital': 'Raleigh',
-        'state': 'North Carolina', 'statehood_order': '12',
-        'statehood_year': '1789'
-    },
-    {
-        'abbreviation': 'NY', 'capital': 'Albany',
-        'state': 'New York', 'statehood_order': '11',
-        'statehood_year': '1788'
-    },
-    {
-        'abbreviation': 'TX', 'capital': 'Austin',
-        'state': 'Texas', 'statehood_order': '28',
-        'statehood_year': '1845'
-    },
-    {
-        'abbreviation': 'NV', 'capital': 'Carson City',
-        'state': 'Nevada', 'statehood_order': '36',
-        'statehood_year': '1864'
-    },
-    {
-        'abbreviation': 'ME', 'capital': 'Augusta',
-        'state': 'Maine', 'statehood_order': '23',
-        'statehood_year': '1820'
-    }]
+      ]
 
-SKILL_TITLE = "United States Quiz Game"
+SKILL_TITLE = "Math Tutor Quiz Game"
 
-WELCOME_MESSAGE = ("Welcome to the United States Quiz Game!  "
+WELCOME_MESSAGE = ("Welcome to the Math Tutor Quiz Game!  "
                    "You can ask me about any of the "
-                   "fifty states and their capitals, or you can ask me to "
+                   "math questions, or you can ask me to "
                    "start a quiz.  What would you like to do? ")
 
-START_QUIZ_MESSAGE = ("OK.  I will ask you 10 questions about the "
-                      "United States. ")
+START_QUIZ_MESSAGE = ("OK.  I will ask you 10 math questions ")
 
-EXIT_SKILL_MESSAGE = ("Thank you for playing the United States Quiz Game!  "
+EXIT_SKILL_MESSAGE = ("Thank you for playing the Math Tutor Quiz Game!  "
                       "Let's play again soon!")
 
-REPROMPT_SPEECH = "Which other state or capital would you like to know about?"
+REPROMPT_SPEECH = "Which math questions would you like to know about?"
 
-HELP_MESSAGE = ("I know lots of things about the United States.  "
-                "You can ask me about a state "
-                "or a capital, and I'll tell you what I know.  "
+HELP_MESSAGE = ("I know lots of things about math.  "
+                "You can ask me about a math question "
+                "and I'll tell you what I know.  "
                 "You can also test your knowledge by asking me to start "
                 "a quiz.  What would you like to do? ")
 
@@ -301,12 +131,12 @@ FALLBACK_ANSWER = (
 
 SCORE = "Your {} score is {} out of {}. "
 
-SPEECH_DESC = (
-    "{} is the {}th state, "
-    "admitted to the Union in {}.  "
-    "The capital of {} is {}, and the "
-    "abbreviation for {} is "
-    "<break strength='strong'/><say-as interpret-as='spell-out'>"
-    "{}</say-as>.  I've added {} to "
-    "your Alexa app.  Which other state or capital would you like to "
-    "know about? ")
+# SPEECH_DESC = (
+#     "{} is the {}th state, "
+#     "admitted to the Union in {}.  "
+#     "The capital of {} is {}, and the "
+#     "abbreviation for {} is "
+#     "<break strength='strong'/><say-as interpret-as='spell-out'>"
+#     "{}</say-as>.  I've added {} to "
+#     "your Alexa app.  Which other state or capital would you like to "
+#     "know about? ")
