13a4b22dc26d6230d6b140efcd30b35b13ee4282
amzjctuan <43017909+amzjctuan@users.noreply.github.com>
2018-12-06 17:35:25 -0500

    data_1.py
    level 2 data sheet

---
+++ lambda/py/alexa/data_1.py
@@ -0,0 +1,133 @@
+ADDITION_LIST = [
+    {
+        'abbreviation': '2', 'answer': '200',
+        'state': '100_+_100', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '161',
+        'state': '125_+_36', 'capital': '4',
+        'statehood_year': '4'
+    },
+    {
+        'abbreviation': '2', 'answer': '518',
+        'state': '393_+_125', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '1212',
+        'state': '978_+_234', 'capital': '4',
+        'statehood_year': '4'
+    },
+    {
+        'abbreviation': '2', 'answer': '1038',
+        'state': '999_+_39', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '1054',
+        'state': '256_+_798', 'capital': '4',
+        'statehood_year': '4'
+    },
+      ]
+
+SUBTRACTION_LIST = [
+    {
+        'abbreviation': '2', 'answer': '0',
+        'state': 'one_minus_one', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '1',
+        'state': 'two_minus_one', 'capital': '4',
+        'statehood_year': '4'
+    },
+    {
+        'abbreviation': '2', 'answer': '28',
+        'state': 'thirty_minus_two', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '30',
+        'state': 'fourty_minus_ten', 'capital': '4',
+        'statehood_year': '4'
+    },
+    {
+        'abbreviation': '2', 'answer': '41',
+        'state': 'fifty_minus_nine', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '49',
+        'state': 'sixty_minus_eleven', 'capital': '4',
+        'statehood_year': '4'
+    },
+      ]
+
+MULTIPLICATION_LIST = [
+    {
+        'abbreviation': '2', 'answer': '1',
+        'state': 'one_multiply_one', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '4',
+        'state': 'two_multiply_two', 'capital': '4',
+        'statehood_year': '4'
+    },
+    {
+        'abbreviation': '2', 'answer': '60',
+        'state': 'thirty_multiply_two', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '400',
+        'state': 'fourty_multiply_ten', 'capital': '4',
+        'statehood_year': '4'
+    },
+    {
+        'abbreviation': '2', 'answer': '450',
+        'state': 'fifty_multiply_nine', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '420',
+        'state': 'sixty_multiply_eleven', 'capital': '4',
+        'statehood_year': '4'
+    },
+      ]
+
+DIVISION_LIST = [
+    {
+        'abbreviation': '2', 'answer': '5',
+        'state': 'ten_divide_two', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '10',
+        'state': 'one-hundred_divide_ten', 'capital': '4',
+        'statehood_year': '4'
+    },
+    {
+        'abbreviation': '2', 'answer': '7',
+        'state': 'forty-nine_divide_seven', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '9',
+        'state': 'eighty-one_divide_nine', 'capital': '4',
+        'statehood_year': '4'
+    },
+    {
+        'abbreviation': '2', 'answer': '7',
+        'state': 'twenty-one_divide_three', 'capital': '2',
+        'statehood_year': '2'
+    },   
+    {
+        'abbreviation': '4', 'answer': '5',
+        'state': 'twenty-five_divide_five', 'capital': '4',
+        'statehood_year': '4'
+    },
+      ]
+
+START_QUIZ_MESSAGE = ("OK. I will ask you 5 extra hard {} math questions ")
